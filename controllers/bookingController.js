74bd94b63f4e31ff2ba54a930786b851aa1da56d
long <hoangphilong2611@gmail.com>
2023-06-12 22:19:04 +0700

    change little

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -11,7 +11,7 @@ exports.getCheckOutSession = catchAsync(async (req, res, next) => {
     const session = await stripe.checkout.sessions.create({
         payment_method_types: ['card'],
         //success_url: `${req.protocol}://${req.get('host')}/?tour=${req.params.tourID}&user=${req.user.id}&price=${tour.price}`,
-        success_url: `${req.protocol}://${req.get('host')}/my-tours?alert=booking`,
+        success_url: `${req.protocol}://${req.get('host')}/my-tours`,
         cancel_url: `${req.protocol}://${req.get('host')}/tour/${tour.slug}`,
         customer_email: req.user.email,
         client_reference_id: req.params.tourID,
@@ -49,7 +49,8 @@ exports.getCheckOutSession = catchAsync(async (req, res, next) => {
 const createBookingCheckout = async session => {
     const tour = session.client_reference_id
     const user = (await User.findOne({ email: session.customer_email })).id
-    const price = session.line_items[0].price_data.unit_amount / 100
+    // const price = session.line_items[0].price_data.unit_amount / 100
+    const price = session.amount_total / 100
     await Booking.create({ tour, user, price })
 }
 
