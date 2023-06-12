3f34eb37555e2e18f3958630925de26b13a284de
long <hoangphilong2611@gmail.com>
2023-06-12 22:21:47 +0700

    add underscore

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -48,7 +48,7 @@ exports.getCheckOutSession = catchAsync(async (req, res, next) => {
 
 const createBookingCheckout = async session => {
     const tour = session.client_reference_id
-    const user = (await User.findOne({ email: session.customer_email })).id
+    const user = (await User.findOne({ email: session.customer_email }))._id
     // const price = session.line_items[0].price_data.unit_amount / 100
     const price = session.amount_total / 100
     await Booking.create({ tour, user, price })
